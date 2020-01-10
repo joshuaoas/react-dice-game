cee1407b2ef0d899991276ea2476761f5a336e7a
Joshua Oas <joshoas@defcon-1.spacesaver.com>
2020-01-10 14:44:54 -0600

    directory structure change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import RollDice from './RollDice';
+import RollDice from './components/dice/RollDice';
 import './App.css';
 
 function App() {
---
+++ src/components/dice/RollDice.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import Die from './Die';
+import './rolldice.css'
+
+export class RollDice extends Component {
+
+    static defaultProps = {
+        sides: ['one', 'two', 'three', 'four', 'five', 'six']
+    };
+
+    constructor(props){
+        super(props);
+        this.state = {
+            die1: 'one',
+            die2: 'one',
+            rolling: false
+        }
+        this.roll = this.roll.bind(this);
+    }
+    roll(){
+        // pick two new rolls
+        const newDie1 = this.props.sides[
+            Math.floor(Math.random() * this.props.sides.length)
+        ];
+        const newDie2 = this.props.sides[
+            Math.floor(Math.random() * this.props.sides.length)
+        ];
+        //set state with new rolls
+        this.setState({die1: newDie1, die2: newDie2, rolling: true});
+
+        // wait one second set rolling false
+        setTimeout(() => {
+            this.setState({rolling: false});
+        }, 1000);
+    }
+
+    render() {
+
+        return (
+            <div className="RollDice">
+                <div class="RollDice-container">
+                    <Die face={this.state.die1} rolling={this.state.rolling} />
+                    <Die face={this.state.die2} rolling={this.state.rolling} />
+                </div>
+                
+                <button onClick={this.roll} disabled={this.state.rolling}>
+                    {this.state.rolling ? 'Rolling....' : 'Roll Dice!'}    
+                </button>
+            </div>
+        )
+    }
+}
+
+export default RollDice;
